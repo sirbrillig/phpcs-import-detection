eb1265f63d61832598767500c1dd5157ce835fa7
Payton Swick <payton@foolord.com>
2018-09-07 13:21:32 -0400

    Tests: add test for allowed unused import

--- tests/Sniffs/Imports/RequireImportsAllowedPatternFixture.php
+++ tests/Sniffs/Imports/RequireImportsAllowedPatternFixture.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace CAR_APP\Vehicles\Greatness;
 
 use function whitelisted_function;
+use function unused_function;
 
 class GreatClass {
 	private function activate() {
