8453157c0d04af0efb6e94d69100dc335bfb71e7
Payton Swick <payton@foolord.com>
2018-09-11 13:00:33 -0400

    Tests: add test for ignoreGlobalsWhenInGlobalScope

---
+++ tests/Sniffs/Imports/GlobalNamespaceFixture.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+use MagicCode\Snacks;
+use function CAR_APP\Functions\whitelisted_function;
+use function CAR_APP\Functions\unused_function; // unused
+use function CAR_APP\Functions\{ three_function, four_function }; // unused
+use function Other_App\{ one_function, two_function };
+
+class GreatClass {
+	private function activate() {
+		whitelisted_function();
+		another_whitelisted_function(); // unimported
+		non_whitelisted_function(); // unimported
+		one_function();
+		two_function();
+		three_function();
+		Snacks\do_that_magic();
+		Cookies\eat_them(); // unimported
+	}
+}
